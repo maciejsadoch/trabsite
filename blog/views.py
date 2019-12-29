5c4ff90a138d3f380fea5eb27cc3cf4d8e1629ee
maciejsadoch <maciej.sadoch@gmail.com>
2019-12-29 16:17:20 +0100

    first chapter - basic blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import View
+from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+class PostDetailView(View):
+    def get(self, request, year, month, day, post):
+        post = Post.published.get(publish__year=year, publish__month=month, publish__day=day, slug=post)
+        return render(request, 'blog/post/detail.html', {'post': post})
